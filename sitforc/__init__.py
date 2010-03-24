9a573caa2e7082421c7b72a98277326710c8f279
ms4py <marc.schlaich@googlemail.com>
2010-03-24 20:50:56 +0100

    Version bump.

--- sitforc/__init__.py
+++ sitforc/__init__.py
@@ -8,7 +8,7 @@ experimental data.
 '''
 
 __author__ = 'Marc Schlaich'
-__version__ = '0.1'
+__version__ = '0.2.0'
 __license__ = 'MIT'
 
 from core import modellib, load_csv, identify_reg, identify_itm
